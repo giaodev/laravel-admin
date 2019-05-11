79a3eab999f4a6f211fd751679705e15be5e48f3
Vu Ngoc Giao <giaovn49@gmail.com>
2019-05-11 14:38:58 +0700

    update email.blade

--- resources/views/default/module/email.blade.php
+++ resources/views/default/module/email.blade.php
@@ -1,9 +1,9 @@
-<h1>Chúc mừng bạn đã đặt hàng thành công</h1>
+<h1 style="color:#000">Chúc mừng bạn đã đặt hàng thành công</h1>
 <h3>Thông tin người nhận hàng: </h3>
-<p><b>Họ tên:</b> {{ $info['name'] }}</p>
-<p><b>Số điện thoại:</b> {{ $info['phone'] }}</p>
-<p><b>Email:</b> {{ $info['email'] }}</p>
-<p><b>Nội dung:</b> {{ $info['note'] }}</p>
+<p style="color:#000"><b>Họ tên:</b> {{ $info['name'] }}</p>
+<p style="color:#000"><b>Số điện thoại:</b> {{ $info['phone'] }}</p>
+<p style="color:#000"><b>Email:</b> {{ $info['email'] }}</p>
+<p style="color:#000"><b>Nội dung:</b> {{ $info['note'] }}</p>
 <h3>Sản phẩm bạn đã đặt: </h3>
     <div class="table">
     <table style="border:1px solid #eee; width:600px">
